e8cdbc528aa67db163d964554c91b92fc9d7722c
ngupta-iwasaki <ngupta-iwasaki@hotmail.com>
2018-06-05 03:18:19 -0700

    fixed config file

--- webpack.config.js
+++ webpack.config.js
@@ -47,7 +47,7 @@ module.exports = (env) => {
       }]
     },
     plugins: [
-      CSSExtract
+      CSSExtract,
       new webpack.DefinePlugin({
         "process.env.FIREBASE_API_KEY": JSON.stringify(process.env.FIREBASE_API_KEY),
         "process.env.FIREBASE_AUTH_DOMAIN": JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
