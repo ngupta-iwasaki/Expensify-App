6e4aa53ef2089c521e6f49e1e5192670ced79eca
ngupta-iwasaki <ngupta-iwasaki@hotmail.com>
2018-06-05 17:54:37 -0700

    added private routes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import {connect} from "react-redux";
 import {NavLink} from "react-router-dom";
+import {startLogout} from "../actions/auth.js";
 
-const Header = () => (
+export const Header = (props) => (
     <header>
         <h1>Expensify</h1>
-        <NavLink to="/" exact={true} activeClassName="isActive">Dashboard</NavLink>
+        <NavLink to="/dashboard" exact={true} activeClassName="isActive">Dashboard</NavLink>
         <NavLink to="/create" activeClassName="isActive">Create Expense</NavLink>
+        <button onClick={props.startLogout}>Log Out</button>
     </header>
 );
 
-export default Header;
+const mapDispatchToProps = (dispatch) => ({
+    startLogout: () => dispatch(startLogout())
+});
+
+export default connect(undefined, mapDispatchToProps)(Header);
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,12 +1,14 @@
 import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
+import thunk from "redux-thunk";
+import authReducer from "../reducers/auth";
 import expenseReducer from "../reducers/expenses";
 import filtersReducer from "../reducers/filters";
-import thunk from "redux-thunk";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export default () => {
         const store = createStore(
         combineReducers({
+            auth: authReducer,
             expenses: expenseReducer,
             filters:  filtersReducer
         }),
