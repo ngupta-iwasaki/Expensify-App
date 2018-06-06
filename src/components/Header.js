6f925a3b489497f758f11bad012d4981af7972e9
ngupta-iwasaki <ngupta-iwasaki@hotmail.com>
2018-06-05 18:10:59 -0700

    added public only routes

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ import {startLogout} from "../actions/auth.js";
 export const Header = (props) => (
     <header>
         <h1>Expensify</h1>
-        <NavLink to="/dashboard" exact={true} activeClassName="isActive">Dashboard</NavLink>
+        <NavLink to="/dashboard" activeClassName="isActive">Dashboard</NavLink>
         <NavLink to="/create" activeClassName="isActive">Create Expense</NavLink>
         <button onClick={props.startLogout}>Log Out</button>
     </header>
